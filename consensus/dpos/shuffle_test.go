3a35c6025338e2556360372cc6c91935fb3d7bba
yujian <yujian_ap@163.com>
2018-02-05 20:34:00 +0800

    add shuffle test,test well

---
+++ consensus/dpos/shuffle_test.go
@@ -0,0 +1,16 @@
+package dpos
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestShuffle(t *testing.T) {
+	var delegateNumber = 4
+	for i := 1;i < 100; i++ {
+		fmt.Println(Shuffle(int64(i),delegateNumber))
+		if i % delegateNumber == 0 {
+			fmt.Println("=======================")
+		}
+	}
+}
